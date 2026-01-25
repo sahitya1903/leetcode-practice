f7b38f73f7ec10fcd82e26c6dd7c0eea9b894e88
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-01-25 17:30:32 +0000

    Leetcode Runtime - 1 ms (99.89%), Memory - 55.9 MB (17.47%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int major=nums[0];
-        int count=0;
-        for(int i=0;i<nums.length;i++){
+        int count=1;
+        for(int i=1;i<nums.length;i++){
             if(nums[i]==major){
                 count++;
-            }else {
+            }else{
                 count--;
-            }
-
-            if(count<0){
-                major=nums[i];
-                count=1;
+                if(count==0){
+                    major=nums[i];
+                    count=1;
+                }
             }
         }
         return major;
