f036d31d4c195cfca2cdff947b54672ef2bb07a2
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-05-17 18:52:00 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 47.9 MB (94.02%)

--- 0792-binary-search/solution.java
+++ 0792-binary-search/solution.java
@@ -3,9 +3,9 @@ class Solution {
         int low=0,high=nums.length-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(target==nums[mid]) return mid;
-            else if(target<nums[mid]) high=mid-1;
-            else low=mid+1;
+            if(nums[mid]==target) return mid;
+            else if(target>nums[mid]) low=mid+1;
+            else high=mid-1; 
         }
         return -1;
     }
