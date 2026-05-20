547558a0fb6583ba46b9ad6b5655ea725ad6a718
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-05-20 14:16:21 +0000

    Leetcode Runtime - 576 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select e.employee_id from employees e
+left join employees m on e.manager_id=m.employee_id
+where (e.salary<30000 and m.employee_id is null and e.manager_id is not null )
+order by employee_id;
