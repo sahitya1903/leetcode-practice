2ac9738a29e072f084e2b71d24c4a4120a83cdb3
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-05-21 13:15:19 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 43.6 MB (75.77%)

---
+++ 0088-merge-sorted-array/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int index=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums2[j]>nums1[i]){
+                nums1[index]=nums2[j];
+                index--; j--;
+            }else{
+                nums1[index]=nums1[i];
+                index--; i--;
+            }
+        }
+
+        while(j>=0){
+            nums1[index]=nums2[j];
+            index--;j--;
+        }
+        
+    }
+}
