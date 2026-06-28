4597fb92f4bebc9601e21e76b9be8057bee0847b
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-06-28 12:10:02 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 43.5 MB (42.34%)

---
+++ 0075-sort-colors/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(nums[i],max);
+        }
+        int [] count=new int[max+1];
+
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]++;
+        }
+
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                nums[j]=i;
+                j++; count[i]--;
+            }
+        }
+    }
+}
