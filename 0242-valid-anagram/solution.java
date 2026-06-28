ec816fedf8c330ba239114159f8b918d105d139a
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-06-28 10:49:28 +0000

    Leetcode Runtime - 3 ms (92.68%), Memory - 46.4 MB (33.47%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,10 +1,9 @@
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char arr1[]=s.toCharArray();
-        char arr2[]=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1,arr2);
+        char [] a=s.toCharArray();
+        char [] b=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
     }
 }
