d95bf4c832206ed0db300cd7f376ffe4cf92b79e
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-07-04 09:25:35 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 42.4 MB (8.60%)

---
+++ 0461-hamming-distance/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int c=0;
+        int n=x^y;
+        while(n!=0){
+            n=n&(n-1);
+            c++;
+        }
+        return c;
+    }
+}
