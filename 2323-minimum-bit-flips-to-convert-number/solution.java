e7955ece4a60dd0ee8b55d5bc170de1a950d06a5
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-07-04 09:30:41 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 42.2 MB (53.42%)

---
+++ 2323-minimum-bit-flips-to-convert-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int temp=start^goal;
+        int ans=0;
+        while(temp!=0){
+            temp=temp&(temp-1);
+            ans++;
+        }
+        return ans;
+    }
+}
