8effaccb3072ed3a4580c44b5fca5d710c322e40
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-07-30 20:53:46 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 42.7 MB (71.82%)

--- 0100-same-tree/solution.java
+++ 0100-same-tree/solution.java
@@ -15,11 +15,9 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null)
-            return true;
-        else if(p!=null && q!=null && p.val==q.val) 
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-        else 
-            return false;
+        if(p==null && q==null) return true;
+        else if(p==null || q==null || p.val!=q.val) return false;
+        if(p.val==q.val) return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return false;
     }
 }
