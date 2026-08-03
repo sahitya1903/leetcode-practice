3fa772dda0cd0e23c90db10393833f8b4fdca9b4
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-08-03 12:36:36 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 41.8 MB (86.09%)

--- 1013-fibonacci-number/solution.java
+++ 1013-fibonacci-number/solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int fib(int n) {
+        // Tabulation
         if(n==0 || n==1) return n;
-        else return fib(n-1)+fib(n-2);
+        int f[]=new int[n+1];
+        f[0]=0;
+        f[1]=1;
+        for(int i=2;i<=n;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        return f[n];
     }
 }
