5862d76338094e2932944b65d4f90c809c2de1c0
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-08-05 20:07:03 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 42.3 MB (17.80%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int helper(int n,int[] dp){
-        if(n<0) return 0;
-        if(n==0) return 1;
-        if(dp[n]!=0) return dp[n];
-        dp[n]= helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
-    }
     public int climbStairs(int n) {
+        if(n==0) return 1;
+        if(n<0) return 0;
         int dp[]=new int[n+1];
-        return helper(n,dp);
+        dp[0]=1;dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
