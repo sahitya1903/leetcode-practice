071b38724153d10ac8e03478f788824088a3c153
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-09-08 19:12:19 +0000

    Leetcode Runtime - 1 ms (99.93%), Memory - 81 MB (5.78%)

--- 0053-maximum-subarray/solution.java
+++ 0053-maximum-subarray/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=0;
-        int maxsum=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            maxsum=Math.max(sum,maxsum);
-            if(sum<0) sum=0;
+        int ms=nums[0],cs=nums[0];
+        for(int i=1;i<nums.length;i++){
+            cs=Math.max(nums[i],cs+nums[i]);
+            ms=Math.max(cs,ms);
         }
-        return maxsum;
+        return ms;
     }
 }
